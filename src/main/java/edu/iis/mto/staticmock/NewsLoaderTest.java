839308c38841722bb059dae6a4404df8b3fb7087
Rafal Dzierzbinski 180157 <dzemba@op.pl>
2015-05-07 18:55:59 +0200

    Dodanie testu zachowania w klasie NewsLoaderTest.java, sprawdzającego ilość wywołań metody getReader

--- src/main/java/edu/iis/mto/staticmock/NewsLoaderTest.java
+++ src/main/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -9,16 +9,27 @@ import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 import edu.iis.mto.staticmock.reader.NewsReader;
+import org.powermock.api.mockito.PowerMockito;
 
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ ConfigurationLoader.class, NewsReaderFactory.class })
 public class NewsLoaderTest {
 
-	@Test
-	public void loadNews_expectedNoneSubscribentContent_and_OnePublicContent() {
-		mockStatic(ConfigurationLoader.class);
+	NewsLoader newsLoader;
+		IncomingNews incomingNews;
+	
+		ConfigurationLoader configurationLoader;
+		NewsReaderFactory newsReaderFactory;
+		Configuration config;
+		NewsReader reader;
+	
+		public void Config() throws Exception {
+	mockStatic(ConfigurationLoader.class);
 		mockStatic(NewsReaderFactory.class);
 
 		NewsLoader newsLoader = new NewsLoader();
@@ -44,6 +55,21 @@ public class NewsLoaderTest {
 		PublishableNews publishableNews = newsLoader.loadNews();
 	}
 
+
+
+	@Test
+	public void loadNews_expectedNoneSubscribentContent_and_OnePublicContent() {
+		PublishableNews publishableNews = newsLoader.loadNews();
+		assertThat(publishableNews.getPublicContent().size(), is(1));
+		assertThat(publishableNews.getSubscribentContent().size(), is(3));
+	}
+
+	@Test
+	public void behaviorTest_getReaderStarts_exptectedOne () throws Exception {
+ 		PublishableNews publishableNews = newsLoader.loadNews();
+		PowerMockito.verifyStatic(Mockito.times(1));
+		NewsReaderFactory.getReader(Mockito.anyString());
+	}
 	private IncomingInfo mockIncomingInfo(SubsciptionType type) {
 
 		IncomingInfo incomingInfo = mock(IncomingInfo.class);
